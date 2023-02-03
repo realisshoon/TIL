e8588f6ce97ae9cfe03b110bcac2a73800c1a5e4
iamseungman <hsgn21@naver.com>
2023-02-03 19:52:52 +0900

    2023.02.03 js 공부

---
+++ momentum/app.js
@@ -0,0 +1,8 @@
+const player={
+    name: "austin",
+    sayHello: function(otherPersonsName){
+        console.log("Hello"+otherPersonsName+"nice to meet you!");
+    },
+};
+
+player.sayHello("lynn");
--- tweetql/server.js
+++ tweetql/server.js
@@ -1,6 +1,19 @@
 import { ApolloServer,gql } from "apollo-server";
+ 
+//pakage.jason에 "type":"module" 추가 안했다면
+// const{ApolloServer.gql}=require{"apollo-server"}
 
-const server = new ApolloServer({})
+const typeDefs=gql`
+     type Tweet{
+        id:ID
+        text:String
+     }
+    type Query {
+        allTweets: [Tweet]
+    }
+`;
+
+const server = new ApolloServer({typeDefs})
 
 server.listen().then(({url})=>{
     console.log('Running on ${url}');
